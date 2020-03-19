9b6007cdfbce1c7fcc59f06967e6d46169772cb1
2099492 <2099492@talnet.com>
2020-03-19 10:35:51 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ var punten = 0;
 
 function quiz() {
   var vrgn = ["parijs", "8", "ijselmeer"];
+  var merken = ["Volkswagen", "Audi", "Opel", "Porsche", "BMW", "Mercedes", "Mercedes-Benz"];
+  var eilanden = ["Texel", "Vlieland", "Terschelling", "Ameland", "Schiermonnikoog"];
   var frank = document.getElementById("frank").value;
   var spin = document.getElementById("spin").value;
   var meer = document.getElementById("meer").value;
+  var merk = document.getElementById("merk").value;
+  var eiland = document.getElementById("eiland").value;
   var antwrdn = [];
 
   antwrdn.push(frank, spin, meer);
@@ -14,5 +18,11 @@ function quiz() {
       punten++;
     }
   }
-  document.write("je hebt ", punten, "punten ");
+  if (merken.includes(merk)) {
+    punten++;
+  }
+  if (eilanden.includes(eiland)) {
+    punten++;
+  }
+  document.write("je hebt ", punten, " punten");
 }
